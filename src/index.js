6ef4ff5779a3ffe63d8868ed9c47227d002d9988
Amritpal Singh <singhamritpal49@gmail.com>
2020-06-19 01:07:19 -0400

    setup Socket.io

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server); 
 
 const port = process.env.PORT || 3000;
 const publicDirectory = path.join(__dirname,'../public');
@@ -9,7 +13,11 @@ const publicDirectory = path.join(__dirname,'../public');
 
 app.use(express.static(publicDirectory));
 
-app.listen(port, () => {
+io.on('connection',()=> {
+    console.log("New Web socket connection")
+})
+
+server.listen(port, () => {
     console.log("Sever is up at " + port);
 })
 
