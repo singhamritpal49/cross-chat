dfece5bd1ff606a1f9ce821ba1c5a0a76efecdfc
Amritpal Singh <singhamritpal49@gmail.com>
2020-07-22 13:13:56 -0400

    Added time stamp for messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,13 +10,29 @@ const $messages = document.querySelector('#messages');
 
 
 const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
+
+
+
+
+
 
 
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
-        message: message
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm A')
     });
+    $messages.insertAdjacentHTML('beforeend',html);
+})
+
+
+socket.on('locationMessage', (url) => {
+    console.log(url);
+    const html = Mustache.render(locationMessageTemplate, {
+        url: url
+    })
     $messages.insertAdjacentHTML('beforeend',html);
 })
 
